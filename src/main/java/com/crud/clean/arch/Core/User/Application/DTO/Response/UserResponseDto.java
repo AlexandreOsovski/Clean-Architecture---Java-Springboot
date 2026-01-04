adc8e8b36cec44d28a978657298c0921762cf03b
Alexandre Osovski <alexandreosovski1@gmail.com>
2026-01-04 19:45:06 +0000

    fix: padronizando retorno dos dados do usuario

--- src/main/java/com/crud/clean/arch/Core/User/Application/DTO/Response/UserResponseDto.java
+++ src/main/java/com/crud/clean/arch/Core/User/Application/DTO/Response/UserResponseDto.java
@@ -3,11 +3,11 @@ package com.crud.clean.arch.Core.User.Application.DTO.Response;
 import java.time.OffsetDateTime;
 
 public record UserResponseDto(
-                Long id,
-                String nome,
-                String sobrenome,
-                String numero_documento,
-                String email,
-                OffsetDateTime createdAt,
-                OffsetDateTime updatedAt) {
+        Long id,
+        String first_name,
+        String last_name,
+        String document_number,
+        String email,
+        OffsetDateTime createdAt,
+        OffsetDateTime updatedAt) {
 }
